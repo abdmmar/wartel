b008d2964c2d78b239c58594d81a9b5f8a9c4c88
abdmmar <opowae909@gmail.com>
2023-09-26 20:20:40 +0700

    feat: add avatar component

---
+++ src/components/avatar.tsx
@@ -0,0 +1,22 @@
+import { generateColorHSL } from '@/utils/toHex'
+import { slate } from '@radix-ui/colors'
+
+export const Avatar = ({ name, children }: { name: string; children: React.ReactNode }) => {
+  return (
+    <div
+      css={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: generateColorHSL(name),
+        borderRadius: '2px',
+        color: slate.slate1,
+        minWidth: '48px',
+        height: '100%',
+        minHeight: '48px',
+      }}
+    >
+      {children}
+    </div>
+  )
+}
