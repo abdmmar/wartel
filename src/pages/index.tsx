2ccdd117b50a5486bb38c9aa55089d42df8716ee
abdmmar <opowae909@gmail.com>
2023-09-27 01:23:28 +0700

    feat: add dropdown menu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,12 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { useToggle } from '@/hooks/useToggle'
 import { useGetContacts } from '@/services/contact'
+import { keyframes } from '@emotion/react'
 import styled from '@emotion/styled'
-import { jade, slate } from '@radix-ui/colors'
+import { jade, slate, yellow } from '@radix-ui/colors'
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 import dynamic from 'next/dynamic'
-import { HiDotsVertical, HiPlus } from 'react-icons/hi'
+import { HiDotsVertical, HiOutlinePencil, HiPlus, HiStar } from 'react-icons/hi'
 
 const EmptyContacts = dynamic(() =>
   import('@/components/ui/empty-state').then((c) => c.EmptyContacts),
@@ -57,9 +59,31 @@ export default function Home() {
                     {contact.phones[0].number}
                   </ContactItemInfoPhoneNumber>
                 </ContactItemInfo>
-                <Button size="icon" variant="ghost">
-                  <HiDotsVertical />
-                </Button>
+                <DropdownMenu.Root>
+                  <DropdownMenu.Trigger asChild>
+                    <Button size="icon" variant="ghost">
+                      <HiDotsVertical />
+                    </Button>
+                  </DropdownMenu.Trigger>
+                  <DropdownMenu.Portal>
+                    <DropdownMenuContent align="end">
+                      <DropdownMenuItem>
+                        <DropdownMenuItemContent>
+                          <HiOutlinePencil style={{ width: '1.2rem', height: '1rem' }} />{' '}
+                          <span>Edit</span>
+                        </DropdownMenuItemContent>
+                      </DropdownMenuItem>
+                      <DropdownMenuItem>
+                        <DropdownMenuItemContent>
+                          <HiStar
+                            style={{ width: '1.2rem', height: '1.2rem', color: yellow.yellow10 }}
+                          />{' '}
+                          <span>Favourite</span>
+                        </DropdownMenuItemContent>
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu.Portal>
+                </DropdownMenu.Root>
               </ContactItemContent>
             </ContactItem>
           ))}
@@ -68,6 +92,75 @@ export default function Home() {
     </Main>
   )
 }
+
+const slideUpAndFade = keyframes({
+  '0%': { opacity: 0, transform: 'translateY(2px)' },
+  '100%': { opacity: 1, transform: 'translateY(0)' },
+})
+
+const slideRightAndFade = keyframes({
+  '0%': { opacity: 0, transform: 'translateX(-2px)' },
+  '100%': { opacity: 1, transform: 'translateX(0)' },
+})
+
+const slideDownAndFade = keyframes({
+  '0%': { opacity: 0, transform: 'translateY(-2px)' },
+  '100%': { opacity: 1, transform: 'translateY(0)' },
+})
+
+const slideLeftAndFade = keyframes({
+  '0%': { opacity: 0, transform: 'translateX(2px)' },
+  '100%': { opacity: 1, transform: 'translateX(0)' },
+})
+
+const DropdownMenuContent = styled(DropdownMenu.Content)`
+  min-width: 120px;
+  background-color: ${slate.slate1};
+  border-radius: 4px;
+  padding: 0.5rem;
+  box-shadow:
+    0px 10px 38px -10px rgba(22, 23, 24, 0.35),
+    0px 10px 20px -15px rgba(22, 23, 24, 0.2);
+  animation-duration: 300ms;
+  animation-timing-function: cubic-bezier(0.16, 1, 0.3, 1);
+  will-change: transform, opacity;
+
+  &[data-state='open'] {
+    &[data-side='top'] {
+      animation-name: ${slideDownAndFade};
+    }
+    &[data-side='right'] {
+      animation-name: ${slideLeftAndFade};
+    }
+    &[data-side='bottom'] {
+      animation-name: ${slideUpAndFade};
+    }
+    &[data-side='left'] {
+      animation-name: ${slideRightAndFade};
+    }
+  }
+`
+const DropdownMenuItem = styled(DropdownMenu.Item)`
+  font-size: 0.875rem;
+  color: ${slate.slate11};
+  margin-bottom: 0.5rem;
+  padding: 0.2rem;
+  border-radius: 2px;
+
+  :last-child {
+    margin-bottom: 0;
+  }
+
+  :hover {
+    background-color: ${slate.slate3};
+    color: ${jade.jade11};
+  }
+`
+const DropdownMenuItemContent = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`
 
 const Main = styled.main`
   background-color: ${slate.slate1};
