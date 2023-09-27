86dcab1db0961115d51f0439b940782a524e146e
abdmmar <opowae909@gmail.com>
2023-09-27 16:20:48 +0700

    feat(loader): add custom color

--- src/components/ui/loader.tsx
+++ src/components/ui/loader.tsx
@@ -1,14 +1,15 @@
 import { keyframes } from '@emotion/react'
 import styled from '@emotion/styled'
+import { slate } from '@radix-ui/colors'
 
 const spin = keyframes`
   0% { transform: rotate(0deg); }
   100% { transform: rotate(360deg); }
 `
-export const Loader = styled.div<{ width?: string; height?: string }>`
+export const Loader = styled.div<{ width?: string; height?: string; color?: string }>`
   width: ${({ width }) => width || '20px'};
   height: ${({ height }) => height || '20px'};
-  border: 3px solid #fff;
+  border: 3px solid ${({ color }) => color || slate.slate1};
   border-bottom-color: transparent;
   border-radius: 50%;
   display: inline-block;
