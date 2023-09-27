0d104d7484336beac6347b1f7f93be7e57fa2673
abdmmar <opowae909@gmail.com>
2023-09-28 01:46:25 +0700

    refactor: rename files

--- src/data/contact/index.ts
+++ src/data/contact/index.ts
@@ -1,6 +1,5 @@
-export * from './DeleteContactMutation';
-export * from './GetContactQuery';
-export * from './GetContactsLocal';
 export * from './create-contact-mutation';
+export * from './delete-contact-mutation';
+export * from './get-contact-query';
 export * from './get-contacts-query';
 
