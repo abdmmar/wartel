82b2a2715b3be0dd5f003aa882f16b33d6754ac6
abdmmar <opowae909@gmail.com>
2023-09-28 13:45:59 +0700

    fix: reset pagination after add/remove favourite

--- src/services/contact/add-favourite-contact-service.ts
+++ src/services/contact/add-favourite-contact-service.ts
@@ -1,4 +1,6 @@
+import { PAGINATION_DEFAULT_VALUE } from "@/constants"
 import { getContactsQueryKey } from "@/data/contact"
+import { getContactsPaginationVar, getFavContactsPaginationVar } from "@/services/contact"
 import { makePersistedVar } from "@/utils/apollo"
 import { useApolloClient, useReactiveVar } from "@apollo/client"
 
@@ -11,11 +13,19 @@ export const useAddFavouriteContact = () => {
 
   const addFavouriteContact = (favouriteId: number) => {
     favouriteContactsVar([favouriteId, ...contacts])
+
+    getFavContactsPaginationVar(PAGINATION_DEFAULT_VALUE)
+    getContactsPaginationVar(PAGINATION_DEFAULT_VALUE)
+
     client.refetchQueries({ include: [getContactsQueryKey] })
   }
 
   const removeFavouriteContact = (favouriteId: number) => {
     favouriteContactsVar(contacts.filter(contactId => contactId !== favouriteId))
+
+    getFavContactsPaginationVar(PAGINATION_DEFAULT_VALUE)
+    getContactsPaginationVar(PAGINATION_DEFAULT_VALUE)
+
     client.refetchQueries({ include: [getContactsQueryKey] })
   }
 
